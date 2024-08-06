6a16073816689f265fa6f61235a9ade025d77e48
rainierviana <rainier.viana@4bears.com.br>
2024-08-06 16:46:59 -0300

    New Dark Mode function and templates for future settings page

--- Triton/src/app/home/home.page.ts
+++ Triton/src/app/home/home.page.ts
@@ -9,12 +9,36 @@ import { MenuController } from '@ionic/angular';
 })
 export class HomePage {
   @ViewChild('mainContent') mainContent!: ElementRef<HTMLDivElement>;
+  darkMode = false;
+
+  toggleDarkMode(event: any) {
+    this.darkMode = event.detail.checked;
+    document.body.classList.toggle('dark-mode', this.darkMode);
+  }
   public menuData: any[] = [];
+
   private navigationStack: any[] = [];
+
   private forwardStack: any[] = []; // Stack to keep track of forward navigation
   showBackButton: boolean = false;
   showForwardButton: boolean = false; // Flag to control forward button visibility
+
   private initialMainContent: string = '';
+
+  public actionSheetButtons = [
+    {
+      text: 'Português',
+      data: {
+        action: 'delete',
+      },
+    },
+    {
+      text: 'English',
+      data: {
+        action: 'share',
+      },
+    },
+  ];
 
   constructor(public http: HttpClient, private menuCtrl: MenuController) {}
 
@@ -55,63 +79,73 @@ export class HomePage {
 
   renderItem(item: any, parentElement: HTMLElement) {
     const newContent = document.createElement('div');
-
-    const link = document.createElement('p');
-    link.textContent = item.title;
-    link.style.textDecoration = 'none';
-    link.style.cursor = 'pointer';
-    link.style.textAlign = 'left';
-    link.addEventListener('click', () => this.handleItemClick(item));
-
+    
+    // Display the title as plain text instead of a clickable link
+    const title = document.createElement('p');
+    title.textContent = item.title;
+    title.style.textDecoration = 'none';
+    title.style.cursor = 'default';
+    title.style.textAlign = 'left';
+    title.style.transition = '0.3s';
+    
     const description = document.createElement('p');
     description.textContent = item.description;
     description.style.textAlign = 'left';
-    newContent.appendChild(link);
+    description.style.transition = '0.3s';
+    newContent.appendChild(title);
     newContent.appendChild(description);
-    parentElement.appendChild(newContent)
-
+    parentElement.appendChild(newContent);
+    
     if (item.childrens && item.childrens.length > 0) {
       const childTable = document.createElement('table');
-      childTable.style.backgroundColor = 'rgb(208,224,227)';
       childTable.style.borderRadius = '1em';
       childTable.style.width = '100%';
-
+      childTable.style.marginBottom = '2em';
+      childTable.style.color= 'var(--content-table-text)';
+      childTable.style.transition = '0.3s';
+    
       item.childrens.forEach((subItem: any) => {
         const childRow = document.createElement('tr');
-
+  
         const childCol1 = document.createElement('td');
         const childUrl = document.createElement('a');
         childUrl.href = subItem.url;
         childUrl.textContent = subItem.title;
         childUrl.style.textDecoration = 'none';
-        childUrl.style.color = 'rgb(0,79,159)';
+        childUrl.style.color = childTable.style.color;
         childUrl.style.textAlign = 'left';
         childCol1.style.textAlign = 'left';
-
+        childCol1.style.paddingLeft = '1.5em';
+        
         if (!subItem.url) {
           childUrl.style.cursor = 'pointer';
           childUrl.addEventListener('click', (event) => {
             event.preventDefault();
             this.FillContent(subItem);
           });
+          childCol1.style.paddingBottom = '1em';
+          childCol1.style.paddingTop = '1em';
         }
-
+  
         childCol1.appendChild(childUrl);
-
+  
         const childCol2 = document.createElement('td');
         childCol2.textContent = subItem.description;
         childCol2.style.padding = '1em';
         childCol2.style.cursor = 'default';
         childCol2.style.textAlign = 'left';
-
+  
         childRow.appendChild(childCol1);
         childRow.appendChild(childCol2);
+  
         childTable.appendChild(childRow);
       });
-
+    
       parentElement.appendChild(childTable);
     }
   }
+  
+  
 
   handleItemClick(item: any) {
     if (item.url) {
