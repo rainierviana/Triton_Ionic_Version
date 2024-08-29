03066be84b113edcf955e24963d14a5b42784d24
rainierviana <rainier.viana@4bears.com.br>
2024-08-29 12:44:51 -0300

    Updated version with working Editor Mode

--- Triton/src/app/app.component.ts
+++ Triton/src/app/app.component.ts
@@ -13,6 +13,8 @@ export class AppComponent {
   constructor(public http:HttpClient) {}
   ngOnInit() {
     this.Initialize();
+    this.applyStoredSettings();   // Apply saved color settings on app initialization
+    this.applyDarkModeSetting();  // Apply saved dark mode setting on app initialization
   }
 
   Initialize() {
@@ -26,6 +28,55 @@ export class AppComponent {
 
   FillContent(item:any) {
     console.log(JSON.stringify('item'))
+  }
+
+  applyStoredSettings() {
+    const root = document.documentElement;
+
+    const backgroundColor = localStorage.getItem('--ion-background-color');
+    if (backgroundColor) {
+      root.style.setProperty('--ion-background-color', backgroundColor);
+    }
+
+    const toolbarColor = localStorage.getItem('--ion-toolbar-background');
+    if (toolbarColor) {
+      root.style.setProperty('--ion-toolbar-background', toolbarColor);
+    }
+
+    const buttonColor = localStorage.getItem('--ion-button-color');
+    if (buttonColor) {
+      root.style.setProperty('--ion-button-color', buttonColor);
+    }
+
+    const tableBackgroundColor = localStorage.getItem('--table-background');
+    if (tableBackgroundColor) {
+      root.style.setProperty('--table-background', tableBackgroundColor);
+    }
+
+    const textColor = localStorage.getItem('--text-color');
+    if (textColor) {
+      root.style.setProperty('--text-color', textColor);
+    }
+
+    const contentTableTextColor = localStorage.getItem('--content-table-text');
+    if (contentTableTextColor) {
+      root.style.setProperty('--content-table-text', contentTableTextColor);
+    }
+
+    const navButtonsColor = localStorage.getItem('--navbuttons');
+    if (navButtonsColor) {
+      root.style.setProperty('--navbuttons', navButtonsColor);
+    }
+  }
+
+  // Apply dark mode setting from localStorage
+  applyDarkModeSetting() {
+    const isDarkMode = localStorage.getItem('darkMode');
+    if (isDarkMode === 'true') {
+      document.body.classList.add('dark-mode');
+    } else {
+      document.body.classList.remove('dark-mode');
+    }
   }
 }
 
--- Triton/src/app/editor/editor.page.ts
+++ Triton/src/app/editor/editor.page.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { DomSanitizer } from '@angular/platform-browser';
-import { Filesystem, Directory, FilesystemEncoding } from '@capacitor/filesystem';
-import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
-
 
 @Component({
   selector: 'app-editor',
@@ -14,55 +11,142 @@ export class EditorPage {
   appTitle: string = '';
   headerImage: string | null | undefined = null;
   footerText: string = '';
-  selectedLanguage: string = 'en';
+  selectedLanguage: string = '';
 
-  constructor(private http: HttpClient, private sanitizer: DomSanitizer) {}
+  public colors: any = {
+    backgroundColor: '',
+    toolbarBackground: '',
+    buttonColor: '',
+    tableBackground: '',
+    textColor: '',
+    contentTableText: '',
+    navbuttons: '',
+    darkModeBackground: '',
+    darkModeToolbar: '',
+    darkModeButtonColor: '',
+    darkModeTableBackground: '',
+    darkModeTextColor: '',
+    darkModeContentTableText: '',
+    darkModeNavbuttons: '',
+  };
+
+  constructor(private http: HttpClient, private sanitizer: DomSanitizer) {
+    this.initializeColors();
+  }
+
+  initializeColors() {
+    const root = document.documentElement;
+
+    this.colors.backgroundColor = getComputedStyle(root)
+      .getPropertyValue('--ion-background-color')
+      .trim();
+    this.colors.toolbarBackground = getComputedStyle(root)
+      .getPropertyValue('--ion-toolbar-background')
+      .trim();
+    this.colors.buttonColor = getComputedStyle(root)
+      .getPropertyValue('--ion-button-color')
+      .trim();
+    this.colors.tableBackground = getComputedStyle(root)
+      .getPropertyValue('--table-background')
+      .trim();
+    this.colors.textColor = getComputedStyle(root)
+      .getPropertyValue('--text-color')
+      .trim();
+    this.colors.contentTableText = getComputedStyle(root)
+      .getPropertyValue('--content-table-text')
+      .trim();
+    this.colors.navbuttons = getComputedStyle(root)
+      .getPropertyValue('--navbuttons')
+      .trim();
+
+    this.colors.darkModeBackground = getComputedStyle(root)
+      .getPropertyValue('--ion-background-color')
+      .trim();
+    this.colors.darkModeToolbar = getComputedStyle(root)
+      .getPropertyValue('--ion-toolbar-background')
+      .trim();
+    this.colors.darkModeButtonColor = getComputedStyle(root)
+      .getPropertyValue('--ion-button-color')
+      .trim();
+    this.colors.darkModeTableBackground = getComputedStyle(root)
+      .getPropertyValue('--table-background')
+      .trim();
+    this.colors.darkModeTextColor = getComputedStyle(root)
+      .getPropertyValue('--text-color')
+      .trim();
+    this.colors.darkModeContentTableText = getComputedStyle(root)
+      .getPropertyValue('--content-table-text')
+      .trim();
+    this.colors.darkModeNavbuttons = getComputedStyle(root)
+      .getPropertyValue('--navbuttons')
+      .trim();
+  }
+
+  // Method to update the color and save to localStorage
+  updateColor(variableName: string, event: Event) {
+    const colorValue = (event.target as HTMLInputElement).value;
+    const root = document.documentElement;
+    root.style.setProperty(variableName, colorValue);
+    this.saveColorSetting(variableName, colorValue);
+  }
+
+  toggleDarkMode(event: any) {
+    const isDarkMode = event.detail.checked;
+    if (isDarkMode) {
+      document.body.classList.add('dark-mode');
+      localStorage.setItem('darkMode', 'true');
+    } else {
+      document.body.classList.remove('dark-mode');
+      localStorage.setItem('darkMode', 'false');
+    }
+  }
+
+  // Save color setting to localStorage
+  saveColorSetting(key: string, value: string) {
+    localStorage.setItem(key, value);
+  }
+
+  // Apply stored settings from localStorage on initialization
+  applyStoredSettings() {
+    const root = document.documentElement;
+
+    const backgroundColor = localStorage.getItem('--ion-background-color');
+    if (backgroundColor) {
+      root.style.setProperty('--ion-background-color', backgroundColor);
+    }
+
+    const toolbarColor = localStorage.getItem('--ion-toolbar-background');
+    if (toolbarColor) {
+      root.style.setProperty('--ion-toolbar-background', toolbarColor);
+    }
+
+    const buttonColor = localStorage.getItem('--ion-button-color');
+    if (buttonColor) {
+      root.style.setProperty('--ion-button-color', buttonColor);
+    }
+
+    const tablebackground = localStorage.getItem('--table-background');
+    if (tablebackground) {
+      root.style.setProperty('--table-background', tablebackground);
+    }
+
+    const text = localStorage.getItem('--text-color');
+    if (text) {
+      root.style.setProperty('--text-color', text);
+    }
+
+    const tabletext = localStorage.getItem('--content-table-text');
+    if (tabletext) {
+      root.style.setProperty('--content-table-text', tabletext);
+    }
+
+    const navbuttons = localStorage.getItem('--navbuttons');
+    if (navbuttons) {
+      root.style.setProperty('--navbuttons', navbuttons);
+    }
+  }
 
   ngOnInit() {
-    this.loadAppConfig();
-  }
-
-  loadAppConfig() {
-    this.http.get('assets/data/appconfig.json').subscribe((config: any) => {
-      this.appTitle = config.appTitle || '';
-      this.footerText = config.footerText || '';
-      this.selectedLanguage = config.defaultLanguage || 'en';
-      this.headerImage = config.headerImage || null;
-    });
-  }
-
-  async saveConfig() {
-    const config = {
-      appTitle: this.appTitle,
-      footerText: this.footerText,
-      defaultLanguage: this.selectedLanguage,
-      headerImage: this.headerImage
-    };
-
-    const configData = JSON.stringify(config);
-
-    await Filesystem.writeFile({
-      path: 'data/appconfig.json',
-      data: configData,
-      directory: Directory.Data,
-      encoding: FilesystemEncoding.UTF8
-    });
-
-    alert('Configuration saved successfully!');
-  }
-
-  async selectImage() {
-    const image = await Camera.getPhoto({
-      resultType: CameraResultType.DataUrl,
-      quality: 90,
-      allowEditing: false,
-      source: CameraSource.Photos
-    });
-
-    this.headerImage = image.dataUrl;
-  }
-
-  getSafeUrl(imageUrl: string) {
-    return this.sanitizer.bypassSecurityTrustUrl(imageUrl);
+    this.applyStoredSettings();
   }
 }
